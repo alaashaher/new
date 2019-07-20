101d1632746f836042b0dbfc2643601c48938e57
alaa shaher <alaashaher12@gmail.com>
2019-07-20 22:33:04 +0200

    show todo and delete them

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+import React, { Component } from 'react';
+import Todos from './todos';
+class App extends Component {
+  state = {
+    todos: [{
+      id: 1,
+      content: 'buy some milk'
+    },
+    {
+      id: 2,
+      content: 'play some game'
+    }]
+  }
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id;
+    });
+    this.setState({
+      todos: todos
+    })
+  }
+  render() {
+    return (
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+      </div>
+    );
+  }
 }
 
 export default App;
